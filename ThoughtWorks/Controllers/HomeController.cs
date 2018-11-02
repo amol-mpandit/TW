7626993cd96e07c965ee0d152460f1960d1ad838
Amol Pandit <amol.mpandit@gmail.com>
2018-11-02 18:10:50 +0530

    TestProjects

--- ThoughtWorks/Controllers/HomeController.cs
+++ ThoughtWorks/Controllers/HomeController.cs
@@ -1,18 +1,101 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using System.Collections.Generic;
+using System.IO;
+using System.Net;
+using System.Text;
 using System.Web.Mvc;
+using Newtonsoft.Json;
 
 namespace ThoughtWorks.Controllers
 {
     public class HomeController : Controller
     {
-        public ActionResult Index()
+        public string Uri = "https://http-hunt.thoughtworks-labs.net/challenge/";
+        
+        public void Get()
         {
-            ViewBag.Title = "Home Page";
+            string uri = "";
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(uri + "input");
+            request.Headers.Add("userid", "MFUksCI1q");
+            
 
-            return View();
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+                if (stream != null)
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var question = reader.ReadToEnd();
+                        var result = Process(question);
+                        Post(result);
+                    }
+        }
+
+        public string[] Process(string input)
+        {
+            var json = JsonConvert.DeserializeObject<Question>(input);
+            var hiddenTools = json.HiddenTools;
+            var tools = json.Tools;
+            var foundTools = new List<string>();
+            foreach (var word in tools)
+            {
+                bool notFound = true;
+                foreach (char charater in word)
+                {
+                    if (!hiddenTools.Contains(charater.ToString()))
+                    {
+                        notFound = true;
+                        break;
+                    }
+                }
+
+                if (!notFound)
+                {
+                    foundTools.Add(word);
+                }
+            }
+            return foundTools.ToArray();
+
+        }
+
+        public void Post(string[] data)
+        {
+            
+            var foundTools = new FoundTools
+            {
+                Tools = data
+            };
+            var toolsFound = JsonConvert.SerializeObject(foundTools);
+            byte[] dataBytes = Encoding.UTF8.GetBytes(toolsFound);
+
+            HttpWebRequest request = (HttpWebRequest)WebRequest.Create(Uri + "output");
+            request.ContentLength = dataBytes.Length;
+            request.ContentType = "application/json; charset=utf-8";
+            request.Method = "POST";
+            request.Headers.Add("userid", "MFUksCI1q");
+
+            using (Stream requestBody = request.GetRequestStream())
+            {
+                requestBody.Write(dataBytes, 0, dataBytes.Length);
+            }
+
+            using (HttpWebResponse response = (HttpWebResponse)request.GetResponse())
+            using (Stream stream = response.GetResponseStream())
+                if (stream != null)
+                    using (StreamReader reader = new StreamReader(stream))
+                    {
+                        var result = reader.ReadToEnd();
+                    }
         }
     }
+
+    public class Question
+    {
+        public string HiddenTools { get; set; }
+        public string[] Tools { get; set; }
+    }
+
+    public class FoundTools
+    {
+        public string[] Tools { get; set; }
+    }
 }
+
---
+++ ThoughtWorksTest/UnitTest1.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Newtonsoft.Json;
+using ThoughtWorks.Controllers;
+
+namespace ThoughtWorksTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestFoundTools()
+        {
+            var input = @"{""hiddenTools"":""opekandifehgujnsr"",""tools"":[""knife"",""guns"",""rope"",""xy"",""and"",""gjkn""]}";
+
+            var result = Process(input);
+            var expected = new [] { "knife", "guns","and" };
+            CollectionAssert.AreEqual(result,expected);
+        }
+
+        public string[] Process(string input)
+        {
+            var json = JsonConvert.DeserializeObject<Question>(input);
+            var hiddenTools = json.HiddenTools;
+            var tools = json.Tools;
+            var foundTools = new List<string>();
+            foreach (var word in tools)
+            {
+                var found = false;
+                var index = 0;
+                foreach (var charater in word)
+                {
+                    if (hiddenTools.LastIndexOf(charater, index) > index)
+                    {
+                        found = true;
+                        index = hiddenTools.IndexOf(charater);
+                    }
+                    else
+                    {
+                        found = false;
+                    }
+                }
+
+                if (found)
+                {
+                    foundTools.Add(word);
+                }
+            }
+            return foundTools.ToArray();
+
+        }
+    }
+}
